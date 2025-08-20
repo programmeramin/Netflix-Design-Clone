98009606bdc8255f96cca02eea8486a10bbf9fe5
Programmeramin <developeramin2615@gmail.com>
2025-08-20 16:53:18 +0600

    fix bg image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import GetStarted from "./GetStarted";
+import BgImage from "../assets/image/banner-img.jpg"
 
 const HeroSection = () => {
   return (
     <>
-      <section className="relative h-screen bg-[url('./src/assets/image/banner-img.jpg')] bg-cover bg-center flex flex-col items-center justify-center text-white ">
+      <section className="relative h-screen bg-cover bg-center flex flex-col items-center justify-center text-white "
+       style={{
+        backgroundImage : `url(${BgImage})`
+       }}>
         <div className="absolute inset-0 bg-black/60 backdrop:blur-sm">
           {/** Backdrop blur */}
             <Header />
